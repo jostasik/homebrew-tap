503072b6b1eb0706cc1ca2dc9881e3669adf4956
joe <31175268+nigification@users.noreply.github.com>
2022-07-10 05:21:35 -0400

    imported source files to repo

--- Casks/nigification-chromium-nosync.rb
+++ Casks/nigification-chromium-nosync.rb
@@ -2,10 +2,10 @@ cask 'nigification-chromium-nosync' do
   version '84.0.4147.89'
   sha256 '1fcb785f59b201962dfe88aab7f48a80a5d292acb53ea25298f0cae0f2b1b859'
 
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r768962-macOS/Chromium.#{version}.nosync.app.zip"
-  appcast 'https://github.com/macchrome/macstable/releases.atom'
+  url "https://github.com/nigification/homebrew-nigification-chromium/releases/download/v#{version}-r768962-macOS/Chromium.#{version}.nosync.app.zip"
+  appcast 'https://github.com/nigification/homebrew-nigification-chromium/releases.atom'
   name 'Chromium'
-  homepage 'https://github.com/macchrome/macstable/releases'
+  homepage 'https://github.com/nigification/homebrew-nigification-chromium/releases'
 
   app 'Chromium.app'
 end
--- Casks/nigification-chromium-ungoogled.rb
+++ Casks/nigification-chromium-ungoogled.rb
@@ -2,10 +2,10 @@ cask 'nigification-chromium-ungoogled' do
   version '95.0.4638.69'
   sha256 '22cb88e2d461ce4e4cc43acd2352e19bcbf12e80618cb99962fdf9d585d0c40f'
 
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r920003-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
-  appcast 'https://github.com/macchrome/macstable/releases.atom'
+  url "https://github.com/nigification/homebrew-nigification-chromium/releases/download/v#{version}-r920003-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
+  appcast 'https://github.com/nigification/homebrew-nigification-chromium/releases.atom'
   name 'Chromium'
-  homepage 'https://github.com/macchrome/macstable/releases'
+  homepage 'https://github.com/nigification/homebrew-nigification-chromium/releases'
 
   app 'Chromium.app'
 end

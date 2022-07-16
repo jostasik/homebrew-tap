10623618e0ea614b75132614b5b4fa151b1faa31
joe <31175268+nigification@users.noreply.github.com>
2022-07-15 23:13:04 -0400

    fixed references again..

--- Casks/gogorama-chromium-ungoogled.rb
+++ Casks/gogorama-chromium-ungoogled.rb
@@ -2,10 +2,10 @@ cask 'gogorama-chromium-ungoogled' do
   version '95.0.4638.69'
   sha256 '22cb88e2d461ce4e4cc43acd2352e19bcbf12e80618cb99962fdf9d585d0c40f'
 
-  url "https://github.com/gogorama/homebrew-gogorama-chromium/releases/download/v#{version}-r920003-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
-  appcast 'https://github.com/gogorama/homebrew-gogorama-chromium/releases.atom'
+  url "https://github.com/gogorama/homebrew-95-chromium/releases/download/v#{version}-r920003-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
+  appcast 'https://github.com/gogorama/homebrew-95-chromium/releases.atom'
   name 'Chromium'
-  homepage 'https://github.com/gogorama/homebrew-gogorama-chromium/releases'
+  homepage 'https://github.com/gogorama/homebrew-95-chromium/releases'
 
   app 'Chromium.app'
 end

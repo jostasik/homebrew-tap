2a23d71245de2cb71efe25a1188ad3ca10a0efbb
joe <31175268+nigification@users.noreply.github.com>
2022-07-15 23:04:14 -0400

    fixed references

---
+++ Casks/gogorama-chromium-nosync.rb
@@ -0,0 +1,11 @@
+cask 'gogorama-chromium-nosync' do
+  version '84.0.4147.89'
+  sha256 '1fcb785f59b201962dfe88aab7f48a80a5d292acb53ea25298f0cae0f2b1b859'
+
+  url "https://github.com/gogorama/homebrew-gogorama-chromium/releases/download/v#{version}-r768962-macOS/Chromium.#{version}.nosync.app.zip"
+  appcast 'https://github.com/gogorama/homebrew-gogorama-chromium/releases.atom'
+  name 'Chromium'
+  homepage 'https://github.com/gogorama/homebrew-gogorama-chromium/releases'
+
+  app 'Chromium.app'
+end
---
+++ Casks/gogorama-chromium-ungoogled.rb
@@ -0,0 +1,11 @@
+cask 'gogorama-chromium-ungoogled' do
+  version '95.0.4638.69'
+  sha256 '22cb88e2d461ce4e4cc43acd2352e19bcbf12e80618cb99962fdf9d585d0c40f'
+
+  url "https://github.com/gogorama/homebrew-gogorama-chromium/releases/download/v#{version}-r920003-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
+  appcast 'https://github.com/gogorama/homebrew-gogorama-chromium/releases.atom'
+  name 'Chromium'
+  homepage 'https://github.com/gogorama/homebrew-gogorama-chromium/releases'
+
+  app 'Chromium.app'
+end

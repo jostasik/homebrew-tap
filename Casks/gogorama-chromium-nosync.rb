08fe095992d959cb0153230851e29785f80462ba
joe <31175268+nigification@users.noreply.github.com>
2022-07-15 23:12:28 -0400

    fixed references again

--- Casks/gogorama-chromium-nosync.rb
+++ Casks/gogorama-chromium-nosync.rb
@@ -2,10 +2,10 @@ cask 'gogorama-chromium-nosync' do
   version '84.0.4147.89'
   sha256 '1fcb785f59b201962dfe88aab7f48a80a5d292acb53ea25298f0cae0f2b1b859'
 
-  url "https://github.com/gogorama/homebrew-gogorama-chromium/releases/download/v#{version}-r768962-macOS/Chromium.#{version}.nosync.app.zip"
-  appcast 'https://github.com/gogorama/homebrew-gogorama-chromium/releases.atom'
+  url "https://github.com/gogorama/homebrew-95-chromium/releases/download/v#{version}-r768962-macOS/Chromium.#{version}.nosync.app.zip"
+  appcast 'https://github.com/gogorama/homebrew-95-chromium/releases.atom'
   name 'Chromium'
-  homepage 'https://github.com/gogorama/homebrew-gogorama-chromium/releases'
+  homepage 'https://github.com/gogorama/homebrew-95-chromium/releases'
 
   app 'Chromium.app'
 end

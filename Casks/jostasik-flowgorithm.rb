7ac3b88ae9e415ccda9c45914cf68bd0fc3c659a
joe <31175268+jostasik@users.noreply.github.com>
2022-12-04 08:03:46 -0500

    Update jostasik-flowgorithm.rb

--- Casks/jostasik-flowgorithm.rb
+++ Casks/jostasik-flowgorithm.rb
@@ -1,6 +1,6 @@
 cask 'jostasik-flowgorithm' do
   version '3.3.1.1'
-  sha256 '2862998b1c1e93c3bdb296a447bbbc0104980532'
+  sha256 'fde4172b0901bc369c7e2b0dc9fde00a6f5f8d6f23513fc1749179f98058657f'
 
   url "https://github.com/jostasik/homebrew-tap/releases/download/flowgorithm-v#{version}/Flowgorithm.v#{version}.zip"
   appcast 'https://github.com/jostasik/homebrew-tap/releases.atom'

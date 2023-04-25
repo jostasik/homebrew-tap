5c6c7aa1f1873ac4450bf958c05998e660cb049f
joe <31175268+jostasik@users.noreply.github.com>
2023-04-24 20:51:30 -0400

    Update jostasik-flowgorithm.rb

--- Casks/jostasik-flowgorithm.rb
+++ Casks/jostasik-flowgorithm.rb
@@ -1,12 +1,12 @@
 cask 'jostasik-flowgorithm' do
   version '3.4.2'
-  sha256 'e7c0515428cb15029bfdc1673ca8f51ac627445a82bef757b54fbd3eb49492ce'
+  sha256 'nocheck'
 
-  url "https://github.com/jostasik/homebrew-tap/releases/download/flowgorithm-v#{version}/Flowgorithm.v#{version}.zip"
+  url "https://github.com/jostasik/homebrew-tap/releases/download/flowgorithm-v#{version}/Flowgorithm-macOS-v#{version}.zip"
   appcast 'https://github.com/jostasik/homebrew-tap/releases.atom'
   name 'Flowgorithm'
   desc "Flowchart Programming Language"
   homepage 'https://github.com/jostasik/homebrew-tap/releases'
 
-  pkg 'Flowgorithm.v.3.4.2.pkg'
+  pkg 'Flowgorithm.v3.4.2.pkg'
 end

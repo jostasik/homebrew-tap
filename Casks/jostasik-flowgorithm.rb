3ca861af1eb9eb10339985954bcecaa4fab95b16
joe <31175268+jostasik@users.noreply.github.com>
2023-05-19 03:23:33 -0400

    Update jostasik-flowgorithm.rb

--- Casks/jostasik-flowgorithm.rb
+++ Casks/jostasik-flowgorithm.rb
@@ -5,12 +5,8 @@ cask 'jostasik-flowgorithm' do
   url "https://github.com/jostasik/homebrew-tap/releases/download/flowgorithm-v#{version}/Flowgorithm_v#{version}.zip"
   appcast 'https://github.com/jostasik/homebrew-tap/releases.atom'
   name 'Flowgorithm'
-  desc 'Flowchart Programming Language'
+  desc "Flowchart Programming Language"
   homepage 'https://github.com/jostasik/homebrew-tap/releases'
-
-  preflight do
-    system_command '/usr/bin/unzip', args: ['-q', "#{staged_path}/Flowgorithm.zip", '-d', staged_path]
-  end
 
   app 'Flowgorithm.app'
 end

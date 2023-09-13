45503a7550016df700413b57fa827d86f368d155
joe <31175268+jostasik@users.noreply.github.com>
2023-09-13 05:16:21 -0400

    updated to livecheck

--- Casks/jostasik-chromium-nosync.rb
+++ Casks/jostasik-chromium-nosync.rb
@@ -3,7 +3,7 @@ cask 'jostasik-chromium-nosync' do
   sha256 '1fcb785f59b201962dfe88aab7f48a80a5d292acb53ea25298f0cae0f2b1b859'
 
   url "https://github.com/jostasik/homebrew-tap/releases/download/v#{version}-r768962-macOS/Chromium.#{version}.nosync.app.zip"
-  appcast 'https://github.com/jostasik/homebrew-tap/releases.atom'
+  livecheck 'https://github.com/jostasik/homebrew-tap/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/jostasik/homebrew-tap/releases'
 
--- Casks/jostasik-flowgorithm@3.3.1.1.rb
+++ Casks/jostasik-flowgorithm@3.3.1.1.rb
@@ -3,7 +3,7 @@ cask 'jostasik-flowgorithm@3.3.1.1' do
   sha256 'fde4172b0901bc369c7e2b0dc9fde00a6f5f8d6f23513fc1749179f98058657f'
 
   url "https://github.com/jostasik/homebrew-tap/releases/download/flowgorithm-v#{version}/Flowgorithm.v#{version}.zip"
-  appcast 'https://github.com/jostasik/homebrew-tap/releases.atom'
+  livecheck 'https://github.com/jostasik/homebrew-tap/releases.atom'
   name 'Flowgorithm'
   desc "Flowchart Programming Language"
   homepage 'https://github.com/jostasik/homebrew-tap/releases'

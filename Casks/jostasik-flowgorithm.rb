62f16a07f72356774b50d9d0986049d7c42873f1
joe <31175268+jostasik@users.noreply.github.com>
2023-09-27 04:37:58 -0400

    Update jostasik-flowgorithm.rb

--- Casks/jostasik-flowgorithm.rb
+++ Casks/jostasik-flowgorithm.rb
@@ -3,10 +3,15 @@ cask 'jostasik-flowgorithm' do
   sha256 '9c1516489ed6db706571546914c0b1ad837cb48341a158e73d5a1cf3039cdea8'
 
   url "https://github.com/jostasik/homebrew-tap/releases/download/flowgorithm-v#{version}/Flowgorithm_v#{version}.zip"
-  appcast 'https://github.com/jostasik/homebrew-tap/releases.atom'
   name 'Flowgorithm'
   desc "Flowchart Programming Language"
   homepage 'https://github.com/jostasik/homebrew-tap/releases'
+
+  livecheck do
+    url "https://github.com/jostasik/homebrew-tap/releases/latest"
+    strategy :page_match
+    regex(/Flowgorithm_v(\d+(?:\.\d+)*)\.zip/i)
+  end
 
   app 'Flowgorithm.app'
 end

e99187cb0f84dd8b7c8323245f2d2d65fdd24151
joe <31175268+jostasik@users.noreply.github.com>
2024-05-27 08:31:42 -0400

    fixed sha256

--- Casks/jostasik-flowgorithm-beta.rb
+++ Casks/jostasik-flowgorithm-beta.rb
@@ -1,6 +1,6 @@
 cask 'jostasik-flowgorithm-beta' do
   version '4.2.0'
-  sha256 '9c1516489ed6db706571546914c0b1ad837cb48341a158e73d5a1cf3039cdea8'
+  sha256 'b1b572ae3dffd1d1ee877f3a0b7cdc934de58158368f5f9cef318273c1108090'
 
   url "https://github.com/jostasik/homebrew-tap/releases/download/flowgorithm-v#{version}/Flowgorithm-v#{version}.zip"
   name 'Flowgorithm'
